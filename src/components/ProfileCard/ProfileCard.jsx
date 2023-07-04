e46002458125a1c2e7558a5b74eead38aa749ecc
slakra144 <slakra144@gmail.com>
2023-07-04 20:12:04 +0530

    Changes till Profile Section
    Ive only changed the global font setting till now

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
+import './ProfileCard.css'
+const ProfileCard = () => {
+  return (
+    <div className='ProfileCard'>
+
+        <div className="ProfileImages">
+        <img src={Cover} alt="" />
+        <img src={Profile} alt="" />
+        </div>
+
+        <div className="ProfileName">
+            <span>Pramit Ki Maal</span>
+            <span>SalesForce Developer</span>
+        </div>
+
+        <div className="followStatus">
+            <hr />
+            <div>
+                <div className="follow">
+                    <span>7,000</span>
+                    <span>Followings</span>
+                </div>
+
+                <div className="vl"></div>
+                
+                <div className="follow">
+                    <span>7</span>
+                    <span>Followers</span>
+                </div>
+
+            </div>
+            <hr />
+        </div>
+
+        <span>
+                My Profile
+        </span>
+
+    </div>
+
+    
+  )
+}
+
+export default ProfileCard
